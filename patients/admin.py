4f98452d79dee25d4805ee08a91e707520bb12e7
codewith-satyam <satyam6453@gmail.com>
2026-09-06 16:16:09 +0530

    fix patient admin

--- patients/admin.py
+++ patients/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Patient
 
 # Register your models here.
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display=('name','age','gender','mobile','created_at')
+    search_fields=('name','mobile')
+    list_filter=('gender','created_at')
+    
+
+
--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+class Patient(models.Model):
+    GENDER_CHOICE = [
+        ('Male','Male'),
+        ('Female','Female'),
+        ('Other','Other'),
+    ]
+    name=models.CharField(max_length=100)
+    age=models.PositiveBigIntegerField()
+    gender=models.CharField(max_length=10,choices=GENDER_CHOICE)
+    mobile=models.CharField(max_length=15)
+    chief_complaint=models.TextField(blank=True,help_text="Maim problem reported by the Patient")
+    created_at=models.DateTimeField(auto_now_add=True)
 
-# Create your models here.
+    def __str__(self):
+        return f"{self.name}-{self.age}"
+    
